34a4aaffccec7c33ea5163b73f124ee8b9b63f70
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2023-05-14 15:22:53 +1000

    :zap: getting it to predict the dates as well

--- catenaminer/loss.py
+++ catenaminer/loss.py
@@ -31,3 +31,25 @@ class FocalLoss(nn.Module):
             loss *= torch.gather(self.weights, -1, target)
 
         return loss.mean()
+
+
+class DateLossMSE(nn.Module):
+    def forward(self, predictions: Tensor, target: Tensor):
+        error_lower = torch.clamp_min(target[:,0]-predictions[:,0], 0.0)
+        error_upper = torch.clamp_min(predictions[:,0] - target[:,1], 0.0)
+        error = error_lower + error_upper
+        return torch.mean(error**2)
+
+
+class CatenaCombinedLoss(nn.Module):
+    def __init__(self, gamma:float=0.0, date_weight:float=0.2, **kwargs):
+        super().__init__(**kwargs)
+        self.date_weight = date_weight
+        self.date_loss = DateLossMSE()
+        self.category_loss = FocalLoss(gamma=gamma) if gamma > 0.0 else nn.CrossEntropyLoss()
+
+    def forward(self, predictions: Tensor, category_target: Tensor, date_range:Tensor):
+        loss = (1.0 - self.date_weight) * self.category_loss(predictions[:,:-1], category_target)
+        loss += self.date_weight * self.date_loss(predictions[:,-1:], date_range)
+        return loss
+
